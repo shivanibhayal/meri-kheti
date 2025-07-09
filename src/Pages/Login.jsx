2c057a974dde7b2bf0448f0a5c66826a09aa1524
gituser <gituser@gmail.com>
2025-07-09 19:06:22 +0530

    adddddd more

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -24,12 +24,15 @@ const handleLogin=(e)=>{
 <form action="/login" onSubmit={handleLogin} className='form-container'>
 <h3 className='fw-bold mb-2'>Login here</h3>
 <label htmlFor='email' className='mt-3'>Email/Phone</label><br/>
-  <input type="text" id='email' autoFocus name="email" value={email} onChange={(e)=>setEmail(e.target.value)} className='border-1 border-light rounded w-100 bg-transparent fw-bold'/><br/>
+  <input type="text" id='email' placeholder='enter email/phone' autoFocus name="email" value={email} onChange={(e)=>setEmail(e.target.value)} className='border-1 border-light rounded w-100 bg-transparent  '/><br/>
   <label htmlFor='password' className='mt-3'>Password</label><br/>
-  <input type="password" id='password' value={password} onChange={(e)=>setPassword(e.target.value)} className='border-1 border-light rounded w-100 fw-bold bg-transparent' name="password"/><br/>
+  <input type="password" id='password' value={password} placeholder='enter password' onChange={(e)=>setPassword(e.target.value)} className='border-1 border-light rounded w-100   bg-transparent' name="password"/><br/>
   <button type="submit" className='border-0 rounded-3  mt-3 fw-bold p-1 loginbtn logbtn'>Login</button>
-
-   <p className=' rounded-3  mt-3 fw-bold text-success  p-1 logbtn rounded registerbackground ' onClick={()=>navigate('/register')}><a className='underline text-success'>For Register</a></p>
+<div className='d-flex  align-items-center justify-content-center mt-3 gap-5'> 
+  <span className=' rounded-3 fw-bold text-success  p-1 logbtn rounded registerbackground' onClick={()=>navigate('/register')}><a className='underline text-success'>For Register</a></span>
+   <span className=' rounded-3  fw-bold text-success  p-1 logbtn rounded registerbackground' onClick={()=>navigate('/register')}><a className='underline text-success'>Forgot Password?</a></span>
+   </div>
+   
 </form>
     </div>
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
             <img src={img} alt="" className={`img-fluid ${styles.navimg}`} />
           </Link>
         </div>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button className="navbar-toggler border-0 shadow-none " type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="col-9 ">
